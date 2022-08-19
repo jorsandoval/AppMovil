af2d928b03ac093dadd172472d00fbf6b77694a5
Jorge Sandoval <82074301+jorsandoval@users.noreply.github.com>
2022-08-18 20:14:23 -0400

    nada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -7,7 +8,8 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: ''
+    path: '',
+    component:LoginComponent
   },
 ];
 
